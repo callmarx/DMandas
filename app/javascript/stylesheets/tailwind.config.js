0e959e24e87268e5996d84ccce35082dab1fc903
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2021-11-11 22:16:56 -0300

    base view template for boards page
     - basead on Adam Wathan live
       url: https://youtu.be/_H38A6E8Nsg

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,10 +1,21 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [
     "./app/**/*.html.erb",
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        teal: colors.teal,
+        amber: colors.amber,
+        rose: colors.rose,
+        indigo: colors.indigo,
+        fuchsia: colors.fuchsia,
+        blueGray: colors.blueGray,
+      }
+    },
   },
   variants: {
     extend: {},
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "welcome#index"
+  get '/board', to: "board#index"
   authenticate :user do
     get '/test', to: "welcome#test"
   end
