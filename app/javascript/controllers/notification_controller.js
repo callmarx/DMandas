82a41c2a583c613cdcae7937cac2a3150a2a4029
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2021-11-25 11:53:14 -0300

    summarize notification_controller.js

--- app/javascript/controllers/notification_controller.js
+++ app/javascript/controllers/notification_controller.js
@@ -4,16 +4,13 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     setTimeout(() => {
-      this.element.classList.remove('hidden');
-      this.element.classList.add(
-        'transition', 'duration-500', 'ease-in-out', 'transform', '-translate-y-10', 'opacity-0'
-      );
+      this.element.classList.remove('-translate-y-10', 'opacity-0');
+      this.element.classList.add('translate-y-4', 'opacity-100');
+    }, 10);
 
-      setTimeout(() => {
-        this.element.classList.remove('-translate-y-10', 'opacity-0');
-        this.element.classList.add('translate-y-4', 'opacity-100');
-      }, 500);
-    }, 100);
+    setTimeout(() => {
+      this.close()
+    }, 4000)
   }
 
   close() {
